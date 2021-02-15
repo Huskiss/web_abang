a1b45e4e11388f7938b88f3927e25796be789233
sh0316 <sh0316@kakao.com>
2021-02-15 15:22:50 +0900

    Calendar db 필드값 수정

--- Web_avangs/calendars/models.py
+++ Web_avangs/calendars/models.py
@@ -7,7 +7,7 @@ class Calendar(models.Model):
     end = models.CharField(max_length=50)
     location = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
+    color = models.CharField(max_length=50)
 
     def __str__(self):
         return self.title
