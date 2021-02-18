2d663d9f7d8362308d53dd7d80f71d88ca5fb70f
sungsu <fkdleks12@naver.com>
2021-02-18 19:24:28 +0900

    lasttime

--- Web_avangs_수정/bookmarks/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'bookmarks'
-
-urlpatterns = [
-    path('', views.bookmarks, name='bookmarks'),
-    path('map/', views.map, name='map')
-]
